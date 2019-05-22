cd6312861fac67885a1de02b53a6f03543a48e09
Noel Mendoza <nmendozavd@gmail.com>
2019-05-21 19:11:00 -0700

    updated with readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Coding Dojo Bootcamp Assignment  
+## Python, Django / Flask / Hello World
+
+### Assignment details  
+Create a server file with one route that sends back a template containing the classic "Hello World".  
+
+Your result should look something like this:  
+
+Hint: If you have only one route that your server is listening for, it must be your root route ("/")  
--- portfolio.py
+++ portfolio.py
@@ -1,3 +1,5 @@
+
+"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -15,5 +17,23 @@ def projects():
 def about():
     return render_template('about.html')
 
+
+app.run(debug=True)
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 app.run(debug=True)
